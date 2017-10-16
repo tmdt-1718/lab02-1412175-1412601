1cfb27e0981da22925e16deb70c0441fba59bf98
Bill <baotuan0.0@gmail.com>
2017-10-16 18:43:02 +0700

    Added devise gem + generate database for user management

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def flash_class(key)
+        case key
+        when "success"
+            "alert-success"
+        when "error"
+            "alert-danger"
+        end
+    end
 end
--- scripts/startup.sh
+++ scripts/startup.sh
@@ -3,4 +3,5 @@
 rm -f /webapp/tmp/pids/server.pid
 bundle install
 rails db:create
+rails db:migrate
 rails s -b 0.0.0.0
