8863a090d6399abdd2312e4265fb957ba0c86477
Bill <baotuan0.0@gmail.com>
2017-10-20 12:07:35 +0700

    Fix minor error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,12 @@
 module ApplicationHelper
     def flash_class(key)
-        if !key
-            "alert-info"
-        else
         case key
         when "success"
             "alert-success"
         when "error"
             "alert-danger"
+        else
+            "alert-info"
         end
     end
 end
